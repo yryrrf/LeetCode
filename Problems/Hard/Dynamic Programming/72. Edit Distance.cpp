5019c57b39406a6ca2c3652aecf8d838370e2e27
Wong Wing Him <31237503+yryrrf@users.noreply.github.com>
2021-09-08 06:54:24 +0800

    Add files via upload

---
+++ Problems/Hard/Dynamic Programming/72. Edit Distance.cpp
@@ -0,0 +1,20 @@
+/*
+Runtime: 16 ms, faster than 45.70% of C++ online submissions for Edit Distance.
+Memory Usage: 9.1 MB, less than 32.06% of C++ online submissions for Edit Distance.
+*/
+class Solution {
+public:
+    int minDistance(string word1, string word2) {
+        vector<vector<int>> dp(word1.size()+1,vector<int>(word2.size()+1,0));
+        for(int i = 0; i<dp.size(); i++) dp[i][0] = i;
+        for(int j = 0; j<dp[0].size(); j++) dp[0][j] = j;
+        
+        for(int i = 1; i<dp.size(); i++){
+            for(int j = 1; j<dp[0].size(); j++){
+                dp[i][j] = min(dp[i-1][j-1],min(dp[i-1][j],dp[i][j-1]))+1;
+                if(word1[i-1] == word2[j-1])  dp[i][j] = min(dp[i][j], dp[i - 1][j - 1]);;
+            }
+        }
+        return dp[dp.size()-1][dp[0].size()-1];
+    }
+};
