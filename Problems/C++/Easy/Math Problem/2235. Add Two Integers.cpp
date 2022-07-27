4371ec04060c2b7d5f836272e569a03fdee190dd
Wong Wing Him <winghimw098@gmail.com>
2022-07-27 23:52:26 +0800

    Add files via upload

---
+++ Problems/C++/Easy/Math Problem/2235. Add Two Integers.cpp
@@ -0,0 +1,10 @@
+/*
+Runtime: 0 ms, faster than 100.00% of C++ online submissions for Add Two Integers.
+Memory Usage: 5.9 MB, less than 23.63% of C++ online submissions for Add Two Integers.
+*/
+class Solution {
+public:
+    int sum(int num1, int num2) {
+        return num1+num2;
+    }
+};
